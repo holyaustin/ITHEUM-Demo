6f04e14428d7c8b4b533176b229a7b6d182050a1
holyaustin <holyaustin@yahoo.com>
2023-10-20 12:59:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Data Wallet App
+
+## Description
+
+This is a simple data wallet that using Itheum SDK to get the data NFT's.
+
+## Instructions
+
+  1. Clone the repository
+  2. Be sure you are in the demos-simple-data-wallet directory
+  4. Run command to install the depedencies: npm install
+  5. Start the app : npm start 
+  6. Connect with your wallet
+  7. Go to Wallet page 
+  8. Browse data nft's
+
+## Learn More
+
+You can learn more about Itheum SDK [Itheum SDK Docs](https://itheum.github.io/sdk-mx-data-nft/modules.html).
+
+![image](https://github.com/Itheum/demos-simple-data-wallet/assets/92076681/28fbb443-c379-4096-b061-da1012092dd1)
+![image](https://github.com/Itheum/demos-simple-data-wallet/assets/92076681/b852fc02-1f46-4020-ab27-135432e295b7)
+![image](https://github.com/Itheum/demos-simple-data-wallet/assets/92076681/583f40e2-e9a4-4b16-9709-267b9cebe4eb)
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+import { DappProvider } from "@multiversx/sdk-dapp/wrappers";
+import { NotificationModal, SignTransactionsModals, TransactionsToastList } from "@multiversx/sdk-dapp/UI";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Layout/Navbar";
+import { apiTimeout, walletConnectV2ProjectId } from "./config";
+import { Footer } from "./components/Layout/Footer";
+import { Home } from "./pages/Home";
+import { Wallet } from "./pages/Wallet";
+import { Content } from "./components/Layout/Content";
+
+function App() {
+  return (
+    <DappProvider
+      environment={"devnet"}
+      customNetworkConfig={{
+        name: "customConfig",
+        apiTimeout,
+        walletConnectV2ProjectId,
+      }}>
+      <TransactionsToastList successfulToastLifetime={1000} customToastClassName="absolute" />
+      <NotificationModal />
+      <SignTransactionsModals className="custom-class-for-modals" />
+      <div className="background bg-no-repeat bg-cover">
+        <div className="bg-gradient-to-b from-gray-900/70 via-[#300171]/50 to-slate-900/70">
+          <div className="flex flex-col min-h-screen text-white">
+            <Navbar />
+            <Content>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/wallet" element={<Wallet />}></Route>
+              </Routes>
+            </Content>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </DappProvider>
+  );
+}
+
+export default App;
